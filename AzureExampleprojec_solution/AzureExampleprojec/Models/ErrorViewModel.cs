e8b9bfd5c74c948e37231bc6c051f6d89fd859cc
keerthana622 <mailtokeerthu62@gmail.com>
2021-05-26 12:58:01 +0530

    AzureSimpleProject

---
+++ AzureExampleprojec_solution/AzureExampleprojec/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AzureExampleprojec.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
